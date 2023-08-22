6f9d105de26b947c2c989cc6c39b873254d6a775
Edison Vargas <helpedison@gmail.com>
2023-08-22 07:18:42 -0500

    new features in change img and fix in the snipet code of css.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,65 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
-import About from './Components/About';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Portfolio from './Components/Portfolio';
-import Resume from './Components/Resume';
-import Testimonials from './Components/Testimonials';
-import './App.css';
+import React, { Component } from "react";
+import $ from "jquery";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Portfolio from "./Components/Portfolio";
+import Resume from "./Components/Resume";
+import Testimonials from "./Components/Testimonials";
+import "./App.css";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      portfolioData: {}
-    }
+      portfolioData: {},
+    };
   }
+
   getPortfolioData() {
     // Fetch request
-    fetch('https://project-profile-business.uc.r.appspot.com/api/myself/data', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        }
+    fetch("https://project-profile-business.uc.r.appspot.com/api/myself/data", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
     })
-    .then(response => {
+      .then((response) => {
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+          throw new Error(`HTTP error! status: ${response.status}`);
         }
         return response.json();
-    })
-    .then(data => {
+      })
+      .then((data) => {
         this.setState({ portfolioData: data });
-    })
-    .catch(error => {
-        console.error("An error occurred while loading the portfolio data: ", error);
+      })
+      .catch((error) => {
+        console.error(
+          "An error occurred while loading the portfolio data: ",
+          error
+        );
         alert("An error occurred. Please check the console for more details.");
-    });
-}
+      });
+  }
 
-
-
-  componentDidMount(){
+  componentDidMount() {
+    /* setTimeout(function () {
+      getPortfolioData();
+    }, 100);*/
     this.getPortfolioData();
   }
+
   render() {
     console.log(this.state.portfolioData);
     return (
       <div className="App">
         <Header data={this.state.portfolioData.main} />
         <About data={this.state.portfolioData.main} />
-        <Resume data={this.state.portfolioData.resume}  />
+        <Resume data={this.state.portfolioData.resume} />
         <Portfolio data={this.state.portfolioData.portfolio} />
-        <Testimonials  data={this.state.portfolioData.testimonials} />
+        <Testimonials data={this.state.portfolioData.testimonials} />
         <Contact data={this.state.portfolioData.main} />
         <Footer />
       </div>
